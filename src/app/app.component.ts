f695571456a71858acf821e84ad0fef9c59c9278
Pedro Uchoa <joaopedrouchoacampos@gmail.com>
2025-10-29 22:29:49 -0300

    Add: Head Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { IntroductionPageComponent } from './pages/introduction-page/introductio
 import { SkillPageComponent } from './pages/skill-page/skill-page.component';
 import { NgOptimizedImage } from '@angular/common';
 import { ProjectPageComponent } from './pages/project-page/project-page.component';
+import { HeadComponent } from './components/head/head.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,IntroductionPageComponent,SkillPageComponent,NgOptimizedImage,ProjectPageComponent],
+  imports: [RouterOutlet,IntroductionPageComponent,SkillPageComponent,NgOptimizedImage,ProjectPageComponent, HeadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
